4b1ffd8f35eb4f46d23eb75ab72dd28749435c30
Phil Manning <phil.manning11@gmail.com>
2020-04-29 16:46:53 +0800

    moving meta to index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -9,6 +9,44 @@ import HeroHeader from '../components/HeroHeader'
 export default {
   metaInfo: {
     title: '#startsmall',
+    meta: [
+      // twitter-card: https://cards-dev.twitter.com/validator
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@jack' },
+      { name: 'twitter:creator', content: '@pj_manning' },
+      {
+        name: 'twitter:title',
+        content: '$1B Fund for global COVID-19 relief.',
+      },
+      {
+        name: 'twitter:description',
+        content:
+          "Jack Dorsey's $1B Square equity fund for global COVID-19 relief, UBI, and girl's health and education.",
+      },
+      {
+        name: 'twitter:image',
+        content:
+          'https://drive.google.com/file/d/1i14aflC71dBBiM9S4J0pyrLrOPgBQt8Y/view?usp=sharing',
+      },
+      // facebook card https://developers.facebook.com/tools/debug/
+      {
+        property: 'og:title',
+        content: '$1B Fund for global COVID-19 relief.',
+      },
+      { property: 'og:type', content: 'website' },
+      {
+        property: 'og:image',
+        content:
+          'https://drive.google.com/file/d/1i14aflC71dBBiM9S4J0pyrLrOPgBQt8Y/view?usp=sharing',
+      },
+      { property: 'og:url', content: 'https://startsmall.llc' },
+      {
+        property: 'og:description',
+        content:
+          "Jack Dorsey's $1B Square equity fund for global COVID-19 relief, UBI, and girl's health and education.",
+      },
+      // { property: 'fb:app_id', content: '416774685599230' },
+    ],
   },
   components: {
     HeroHeader,
