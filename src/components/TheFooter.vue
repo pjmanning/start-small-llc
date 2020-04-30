155e95a6f1037bad4583019633e02fea7d8a9f85
Phil Manning <phil.manning11@gmail.com>
2020-05-01 00:26:07 +0800

    footer changes

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,7 +3,7 @@
     <div
       class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8"
     >
-      <div class="flex justify-center md:order-2">
+      <!-- <div class="flex justify-center md:order-2">
         <a href="#" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">Facebook</span>
           <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -56,24 +56,80 @@
             />
           </svg>
         </a>
-      </div>
-      <div class="mt-8 md:mt-0 md:order-1">
+      </div>-->
+      <div class="mt-0 mx-auto">
         <p class="text-center text-base leading-6 text-gray-400">
           Made in Bali 🏄🏻‍♂️ by
           <a
             href="https://pjmanning.dev"
             target="_blank"
             class="text-blue-600 hover:text-blue-900"
-            >PJ Manning</a
-          >
+          >PJ Manning</a>
         </p>
       </div>
+      <!-- <div class="coffee md:order-2">
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext"
+          rel="stylesheet"
+        />
+        <a class="bmc-button" target="_blank" href="https://www.buymeacoffee.com/csDuFKC">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg"
+            alt="Buy me a coffee"
+          />
+          <span style="margin-left:15px;font-size:19px !important;">Buy me a coffee</span>
+        </a>
+      </div>-->
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+.bmc-button img {
+  height: 34px !important;
+  width: 35px !important;
+  margin-bottom: 1px !important;
+  box-shadow: none !important;
+  border: none !important;
+  vertical-align: middle !important;
+}
+.bmc-button {
+  padding: 7px 10px 7px 10px !important;
+  line-height: 35px !important;
+  height: 51px !important;
+  min-width: 217px !important;
+  text-decoration: none !important;
+  display: inline-flex !important;
+  color: #ffffff !important;
+  background-color: #5f7fff !important;
+  border-radius: 5px !important;
+  border: 1px solid transparent !important;
+  padding: 7px 10px 7px 10px !important;
+  font-size: 20px !important;
+  letter-spacing: -0.08px !important;
+  box-shadow: 0px 1px 2px rgba(190, 190, 190, 0.5) !important;
+  -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  margin: 0 auto !important;
+  font-family: "Lato", sans-serif !important;
+  -webkit-box-sizing: border-box !important;
+  box-sizing: border-box !important;
+  -o-transition: 0.3s all linear !important;
+  -webkit-transition: 0.3s all linear !important;
+  -moz-transition: 0.3s all linear !important;
+  -ms-transition: 0.3s all linear !important;
+  transition: 0.3s all linear !important;
+}
+.bmc-button:hover,
+.bmc-button:active,
+.bmc-button:focus {
+  -webkit-box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  text-decoration: none !important;
+  box-shadow: 0px 1px 2px 2px rgba(190, 190, 190, 0.5) !important;
+  opacity: 0.85 !important;
+  color: #ffffff !important;
+}
+</style>
