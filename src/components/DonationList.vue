26868730e7c3ee8008e7a197c3cab6450589d561
Phil Manning <phil.manning11@gmail.com>
2020-05-20 11:12:36 +0800

    cursor pointer on sorting

--- src/components/DonationList.vue
+++ src/components/DonationList.vue
@@ -50,7 +50,7 @@
                     <div class="py-1">
                       <div
                         @click="filterByDate"
-                        class="flex items-center px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                        class="flex items-center px-4 py-2 text-sm leading-5 text-gray-700 cursor-pointer hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
                       >
                         <svg
                           fill="none"
@@ -69,7 +69,7 @@
                       </div>
                       <div
                         @click="filterByAmount"
-                        class="flex items-center px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
+                        class="flex items-center px-4 py-2 text-sm leading-5 text-gray-700 cursor-pointer hover:bg-gray-100 hover:text-gray-900 focus:outline-none focus:bg-gray-100 focus:text-gray-900"
                       >
                         <svg
                           fill="none"
