81f596c23e89996ad49c4d3c1fd7aa7d9db951b3
PJ Manning <phil.manning11@gmail.com>
2022-01-08 21:42:59 -0500

    adding a scroll to top button on index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -11,6 +11,28 @@
         <FAQList id="faq" />
         <HomeCTA id="cta" />
         <!-- <SubmissionList /> -->
+        <transition name="fade">
+            <div
+                id="pagetop"
+                class="fixed bg-blue-600 rounded-full cursor-pointer bottom-6 right-6 hover:bg-blue-500"
+                v-show="scY > 300"
+                @click="toTop"
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="48"
+                    height="48"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="#fff"
+                    stroke-width="1"
+                    stroke-linecap="square"
+                    stroke-linejoin="arcs"
+                >
+                    <path d="M18 15l-6-6-6 6" />
+                </svg>
+            </div>
+        </transition>
         <TheFooter />
     </Layout>
 </template>
@@ -57,6 +79,35 @@ export default {
         SubmissionList,
         TheFooter
     },
+
+    data() {
+        return {
+            scTimer: 0,
+            scY: 0
+        }
+    },
+
+    mounted() {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+
+    methods: {
+        handleScroll: function() {
+            if (this.scTimer) return
+            this.scTimer = setTimeout(() => {
+                this.scY = window.scrollY
+                clearTimeout(this.scTimer)
+                this.scTimer = 0
+            }, 100)
+        },
+        toTop: function() {
+            window.scrollTo({
+                top: 0,
+                behavior: 'smooth'
+            })
+        }
+    },
+
     metaInfo: {
         title: '#startsmall',
         meta: [
